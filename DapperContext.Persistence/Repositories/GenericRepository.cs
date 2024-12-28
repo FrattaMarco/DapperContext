125e8d41c6ae48f0f6d475953588b24fa0109e24
FrattaMarco <marco-fratta@hotmail.it>
2024-12-28 17:26:48 +0100

    Implementazione libreria

--- DapperContext.Persistence/Repositories/GenericRepository.cs
+++ DapperContext.Persistence/Repositories/GenericRepository.cs
@@ -8,14 +8,30 @@ namespace DapperContext.Persistence.Repositories
     public class GenericRepository(ContextDapper contextDapper) : IGenericRepository
     {
         private readonly ContextDapper _contextDapper = contextDapper ?? throw new ArgumentNullException(nameof(contextDapper));
-        public IDbTransaction Transaction => _contextDapper.Transaction!;
+        public IDbTransaction Transaction => _contextDapper.Transaction!;      
+        public async Task<bool> ExecuteStoredProcedureAsyncByTvp<T>(string spName, string tvpName, string tableParameterName, IEnumerable<T> parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure) where T : class
+        {
+            DataTable dt = BuildTvp(parameters);
+            var dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add(tvpName, dt.AsTableValuedParameter(tableParameterName));
 
-        public async Task<IEnumerable<T>> GetAllAsyncWithStoredProcedure<T>(string SPName, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure) where T : class
+            try
+            {
+                await _contextDapper.Connection!.ExecuteAsync(spName, dynamicParameters, transaction: _contextDapper.Transaction, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return true;
+        }
+        public async Task<IEnumerable<T>> GetAllAsyncWithStoredProcedure<T>(string spName, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure) where T : class
         {
             IEnumerable<T> result;
             try
             {
-                result = await _contextDapper.Connection!.QueryAsync<T>(SPName, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
+                result = await _contextDapper.Connection!.QueryAsync<T>(spName, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
             }
             catch (Exception)
             {
@@ -23,13 +39,12 @@ namespace DapperContext.Persistence.Repositories
             }
             return result;
         }
-
-        public async Task<bool> QueryAddAsyncByStoredProcedure(string SPName, object parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure)
+        public async Task<bool> QueryAddAsyncByStoredProcedure(string spName, object parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure)
         {
             long affectedRows;
             try
             {
-                affectedRows = await _contextDapper.Connection!.ExecuteAsync(SPName, parameters, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
+                affectedRows = await _contextDapper.Connection!.ExecuteAsync(spName, parameters, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
             }
             catch (Exception)
             {
@@ -37,13 +52,12 @@ namespace DapperContext.Persistence.Repositories
             }
             return affectedRows > 0;
         }
-
-        public async Task<bool> QueryDeleteAsyncByStoredProcedure(string SPName, object parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure)
+        public async Task<bool> QueryDeleteAsyncByStoredProcedure(string spName, object parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure)
         {
             long affectedRows;
             try
             {
-                affectedRows = await _contextDapper.Connection!.ExecuteAsync(SPName, parameters, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
+                affectedRows = await _contextDapper.Connection!.ExecuteAsync(spName, parameters, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
             }
             catch (Exception)
             {
@@ -51,13 +65,12 @@ namespace DapperContext.Persistence.Repositories
             }
             return affectedRows > 0;
         }
-
-        public async Task<IEnumerable<T>> QueryGetAsyncByStoredProcedure<T>(string SPName, object parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure) where T : class
+        public async Task<IEnumerable<T>> QueryGetAsyncByStoredProcedure<T>(string spName, object parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure) where T : class
         {
             IEnumerable<T> result;
             try
             {
-                result = await _contextDapper.Connection!.QueryAsync<T>(SPName, parameters, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
+                result = await _contextDapper.Connection!.QueryAsync<T>(spName, parameters, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
             }
             catch (Exception)
             {
@@ -65,20 +78,35 @@ namespace DapperContext.Persistence.Repositories
             }
             return result;
         }
-
-        public async Task<T> QueryUpdateAsyncByStoredProcedure<T>(string SPName, object parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure) where T : class
+        public async Task<T> QueryUpdateAsyncByStoredProcedure<T>(string spName, object parameters, CommandType commandTypeStoredProcedure = CommandType.StoredProcedure) where T : class
         {
-            T user;
+            T updatedObject;
             try
             {
-                user = await _contextDapper.Connection!.QuerySingleAsync<T>(SPName, parameters, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
+                updatedObject = await _contextDapper.Connection!.QuerySingleAsync<T>(spName, parameters, commandType: commandTypeStoredProcedure, transaction: _contextDapper.Transaction);
             }
             catch (Exception)
             {
                 throw;
             }
 
-            return user!;
+            return updatedObject!;
+        }
+        public static DataTable BuildTvp<T>(IEnumerable<T> parameters) where T : class
+        {
+            DataTable dt = new();
+            var properties = typeof(T).GetProperties().ToList();
+
+            properties.ForEach(x => dt.Columns.Add(x.Name, Nullable.GetUnderlyingType(x.PropertyType) ?? x.PropertyType));
+
+            parameters.ToList().ForEach(x =>
+            {
+                DataRow row = dt.NewRow();
+                properties.ForEach(p => row[p.Name] = p.GetValue(x) ?? DBNull.Value);
+                dt.Rows.Add(row);
+            });
+
+            return dt;
         }
     }
 }
--- DapperContext/Repositories/IGenericRepository.cs
+++ DapperContext/Repositories/IGenericRepository.cs
@@ -5,10 +5,11 @@ namespace DapperContext.Application.Repositories
     public interface IGenericRepository
     {
         IDbTransaction Transaction { get; }
-        Task<IEnumerable<T>> GetAllAsyncWithStoredProcedure<T>(string SPName, CommandType commandType) where T : class;
-        Task<IEnumerable<T>> QueryGetAsyncByStoredProcedure<T>(string SPName, object parameters, CommandType commandType) where T : class;
-        Task<bool> QueryDeleteAsyncByStoredProcedure(string SPName, object parameters, CommandType commandType);
-        Task<bool> QueryAddAsyncByStoredProcedure(string SPName, object parameters, CommandType commandType);
-        Task<T> QueryUpdateAsyncByStoredProcedure<T>(string SPName, object parameters, CommandType commandType) where T : class;
+        Task<IEnumerable<T>> GetAllAsyncWithStoredProcedure<T>(string spName, CommandType commandType) where T : class;
+        Task<IEnumerable<T>> QueryGetAsyncByStoredProcedure<T>(string spName, object parameters, CommandType commandType) where T : class;
+        Task<bool> QueryDeleteAsyncByStoredProcedure(string spName, object parameters, CommandType commandType);
+        Task<bool> QueryAddAsyncByStoredProcedure(string spName, object parameters, CommandType commandType);
+        Task<T> QueryUpdateAsyncByStoredProcedure<T>(string spName, object parameters, CommandType commandType) where T : class;
+        Task<bool> ExecuteStoredProcedureAsyncByTvp<T>(string spName, string tableParameterName, string tvpName, IEnumerable<T> parameters, CommandType commandType) where T : class;
     }
 }
